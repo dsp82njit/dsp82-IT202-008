a1f6e03f7fcfc77602db59900aeaf0cfcf492821
dsp82njit <dsp82@njit.edu>
2024-02-05 15:44:00 -0500

    save changes

--- public_html/problem2.php
+++ public_html/problem2.php
@@ -8,6 +8,12 @@ function getTotal($arr) {
     $total = 0.00;
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO do adding here
+    foreach ($arr as $value){
+        $total += $value;
+    }
+
+    $total = round($total, 2);
+    echo "The total is " . $total;
     //TODO do rounding stuff here (round to two decimals i.e., 0.10, 0.01, 0.00)
     echo "The total is " . var_export($total, true);
 }
